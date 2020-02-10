0cedf25ae96498e76103ac56501010b1dbc5450e
Andrea Bender <andreab.1726@gmail.com>
2020-02-10 12:08:21 -0700

    router added to app

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const ClimbRouter = require('./climbs/climbs-router');
 
 const { NODE_ENV } = require('./config');
 const app = express();
@@ -16,15 +17,17 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use(function errorHandler(error, req, res, next) {
-	let response;
-	if (NODE_ENV === 'production') {
-		response = { error: { message: 'server error' } };
-	} else {
-		console.error(error);
-		response = { message: error.message, error };
-	}
-	res.status(500).json(response);
-});
+app.use('/api/climbs', ClimbRouter);
+
+// app.use(function errorHandler(error, req, res, next) {
+// 	let response;
+// 	if (NODE_ENV === 'production') {
+// 		response = { error: { message: 'server error' } };
+// 	} else {
+// 		console.error(error);
+// 		response = { message: error.message, error };
+// 	}
+// 	res.status(500).json(response);
+// });
 
 module.exports = app;
